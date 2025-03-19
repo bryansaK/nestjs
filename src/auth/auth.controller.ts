3cd9214fe473d879be60996ad1fe024b2d70be2b
Bryan Sa <bryan.sa.sb@gmail.com>
2025-03-19 16:39:58 +0100

    feat(USER-ENTITY): finalisation de l'auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,30 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { loginAuthDto } from './dto/login-auth.dto';
-import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { UserService } from 'src/user/user.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) { }
+    constructor(private readonly authService: AuthService, private readonly userService: UserService) { }
 
-    @Post()
-    async login(@Body() loginRequest: loginAuthDto) {
+    @Post('/signin')
+    async signin(@Body() loginRequest: loginAuthDto) {
 
-        const checkUser
+        const matchingUser = await this.authService.checkUserLogin(loginRequest);
+        if (!matchingUser) {
+            throw new HttpException('Email ou mdp pas bon', HttpStatus.BAD_REQUEST)
+        }
+        const token = this.authService.getLoginToken(matchingUser)
+        return (token)
+    }
+
+    @Post('/signup')
+    async signup(@Body() userCreationRequest: CreateUserDto) {
+        const checkMail = await this.userService.findOneByEmail(userCreationRequest.email)
+        if (checkMail) {
+            throw new HttpException('Email deja pris', HttpStatus.BAD_REQUEST);
+        }
+        return this.userService.create(userCreationRequest);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { loginAuthDto } from './dto/login-auth.dto';
-
+import * as bcrypt from 'bcrypt';
+import { User } from 'src/user/entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
 
-     constructor(private readonly userService: UserService) { }
-    
+    constructor(private readonly userService: UserService, private readonly jwtService: JwtService) { }
 
-    async checkUser(loginAuthDto: loginAuthDto) {
+
+    async checkUserLogin(loginAuthDto: loginAuthDto) {
         const user = await this.userService.findOneByEmail(loginAuthDto.email);
         if (!user) {
-            return false
+            return null
         }
-        const Mathpwd = await 
+        const matchPwd = await bcrypt.compare(user.password, loginAuthDto.password)
+        if (!matchPwd) {
+            return null
+        }
+        return user
+    }
+
+    async getLoginToken(user: User) {
+    const payload = { sub: user.id, email: user.email };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
     }
 }
